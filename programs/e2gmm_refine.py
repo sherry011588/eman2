b263bbdd88e9a19e5147a12ed2392aafe4e10807
sherry011588 <69136343+sherry011588@users.noreply.github.com>
2022-12-20 13:24:56 +0800

    Update e2gmm_refine.py

--- programs/e2gmm_refine.py
+++ programs/e2gmm_refine.py
@@ -336,6 +336,7 @@ def build_encoder(mid=512, nout=4, conv=False, ninp=-1):
 	if conv:
 		ss=64
 		x = tf.keras.layers.Flatten()(inputs)
+		x = tf.keras.layers.Flatten()(inputs)
 		x = tf.keras.layers.Dense(ss*ss, kernel_regularizer=l2)(x)
 		x = tf.keras.layers.Reshape((ss, ss, 1))(x)
 
