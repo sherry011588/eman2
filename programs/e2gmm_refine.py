b509febdb8cb86f7987391891d6b2ce302f37b34
sherry011588 <69136343+sherry011588@users.noreply.github.com>
2022-12-21 13:26:52 +0800

    Update e2gmm_refine.py

--- programs/e2gmm_refine.py
+++ programs/e2gmm_refine.py
@@ -758,8 +758,8 @@ def train_heterg(trainset, pts, encode_model, decode_model, params, options):
 				cl=tf.reduce_mean(tf.maximum(cl-1,0))
 				
 				##add mean log var  ninp=mid.shape[1]
-				z_mean = tf.keras.layers.Dense(ninp, name="z_mean")(conf)
-				z_log_var = tf.keras.layers.Dense(ninp, name="z_log_var")(conf)
+				z_mean = tf.keras.layers.Dense(mid.shape[1], name="z_mean")(conf)
+				z_log_var = tf.keras.layers.Dense(mid.shape[1], name="z_log_var")(conf)
 				conf = Sampling()([z_mean, z_log_var])
 				
 				
